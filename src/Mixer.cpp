22e0575238d323306ef6292e9b34fdcef70eb380
Stan <stan@stansa.dev>
2023-05-30 09:58:12 -0700

    init commit

---
+++ src/Mixer.cpp
@@ -0,0 +1,15 @@
+/*
+ * Project: Mixer Lib
+ * File: src/Mixer.cpp
+ *
+ * Copyright 2023 Kita LLC
+ */
+
+#ifndef SRC_MIXER_H_
+#define SRC_MIXER_H_
+
+// Top-level Mixer class
+class Mixer {
+};
+
+#endif  // SRC_MIXER_H_
